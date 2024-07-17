8a607056af2946eeae0a3fbf23417826ab2c8c22
Geovanny <geovanny.alcon2@gmail.com>
2024-07-17 08:49:06 -0600

    Probando Cambios

--- proyecto1/Program.cs
+++ proyecto1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Threading.Channels;
+class Program
+{
+    public static void main(String[] arg)
+    {
+        Persona people = new Persona();
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Listo...");
+        people.Saludo();
+
+    }
+
+}
+
+class Persona
+{
+    public void Saludo() {
+        Console.WriteLine("Wenasss :3");
+    }
+}
